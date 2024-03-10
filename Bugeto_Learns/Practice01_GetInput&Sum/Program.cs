eb7f7a5caa05ed624202505b0e38de4c84ca9411
Hossein Eskandary <unity.esk73@gmail.com>
2024-03-10 22:42:17 +0330

    تمرین اول - دریافت ورودی و جمع آن ها

---
+++ Bugeto_Learns/Practice01_GetInput&Sum/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Practice01_GetInput_Sum
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Num1:");
+            int Num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please Enter Num2:");
+            int Num2 = int.Parse(Console.ReadLine());
+
+            int Sum = Num1 + Num2;
+            //Console.WriteLine($"Sum is: {Sum}");
+            Console.WriteLine("Sum is:" + " " +  Sum);
+
+            Console.ReadKey();
+        }
+    }
+}
